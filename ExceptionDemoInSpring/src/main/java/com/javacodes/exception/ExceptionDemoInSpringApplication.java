e73f31eea7086948ba4be144a7627997f83b4fb1
Malleswara Rao <ch.malli1203@gmail.com>
2021-01-30 16:44:12 +0530

    ExceptionHandlingSpring

---
+++ ExceptionDemoInSpring/src/main/java/com/javacodes/exception/ExceptionDemoInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.javacodes.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionDemoInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionDemoInSpringApplication.class, args);
+	}
+
+}
---
+++ ExceptionDemoInSpring/src/test/java/com/javacodes/exception/ExceptionDemoInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacodes.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionDemoInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
